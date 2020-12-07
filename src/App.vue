a44e6594c077a66fc3ee4c1a057690e333820f2e
HuaGuoSan <1135447478@qq.com>
2020-12-07 15:22:10 +0800

    tabbar function

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    
+    <tab-bar-show></tab-bar-show>
   </div>
-  <router-view/>
 </template>
 
+<script>
+  import TabBarShow from 'components/content/tabbar/TabBarShow.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      TabBarShow
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+	@import url("assets/css/base.css");
 </style>
---
+++ src/components/content/tabbar/TabBarShow.vue
@@ -0,0 +1,70 @@
+<template>
+	<div>
+		<router-view></router-view>
+		<tab-bar>
+			<tab-bar-item path="/home" activeColor="red">
+				<template #item-icon>
+					<img src="~assets/img/tabbar/home.png" alt="" />
+				</template>
+				<template #item-icon-active>
+					<img src="~assets/img/tabbar/home_active.png" alt="" />
+				</template>
+				<template #item-text>
+					<div>首页</div>
+				</template>
+			</tab-bar-item>
+
+			<tab-bar-item path="/category" activeColor="red">
+				<template #item-icon>
+					<img src="~assets/img/tabbar/category.png" alt="" />
+				</template>
+				<template #item-icon-active>
+					<img src="~assets/img/tabbar/category_active.png" alt="" />
+				</template>
+				<template #item-text>
+					<div>分类</div>
+				</template>
+			</tab-bar-item>
+
+			<tab-bar-item path="/shopcar" activeColor="red">
+				<template #item-icon>
+					<img src="~assets/img/tabbar/shop_car.png" alt="" />
+				</template>
+				<template #item-icon-active>
+					<img src="~assets/img/tabbar/shop_car_active.png" alt="" />
+				</template>
+				<template #item-text>
+					<div>购物车</div>
+				</template>
+			</tab-bar-item>
+
+			<tab-bar-item path="/profile" activeColor="red">
+				<template #item-icon>
+					<img src="~assets/img/tabbar/profile.png" alt="" />
+				</template>
+				<template #item-icon-active>
+					<img src="~assets/img/tabbar/profile_active.png" alt="" />
+				</template>
+				<template #item-text>
+					<div>我的</div>
+				</template>
+			</tab-bar-item>
+		</tab-bar>
+	</div>
+</template>
+
+<script>
+	import TabBar from 'components/common/tabbar/TabBar.vue'
+	import TabBarItem from 'components/common/tabbar/TabBarItem.vue'
+
+	export default {
+		name: 'TabBarShow',
+		components: {
+			TabBar,
+			TabBarItem
+		}
+	}
+</script>
+
+<style>
+</style>
